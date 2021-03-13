f0493de6f6d4d5a7cdb50a61a8c180cd7c8b045b
Jesse Goodier <31039225+jessegoodier@users.noreply.github.com>
2021-03-13 17:27:21 -0500

    check vm.max_map_count before trying to bring up

--- run.sh
+++ run.sh
@@ -2,4 +2,17 @@
 set -e
 source .env
 perl -i -pe s/yourpasswordhere/$PASSWORD/g ./fluentd/fluent-app-protect.conf
+
+if [ `cat /proc/sys/vm/max_map_count` -lt 262144 ];
+
+then
+echo "max_map_count too low. run:"
+echo "sudo sysctl -w vm.max_map_count=262144"
+echo "and this to persist reboot:"
+echo "\"echo \"vm.max_map_count=262144\" | sudo tee -a /etc/sysctl.conf"
+echo "then run this script again"
+
+else
 docker-compose up --build
+
+fi
