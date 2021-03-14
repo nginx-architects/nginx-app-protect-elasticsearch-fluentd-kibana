70a049c04db30df36957bba59fc428b52807bcd0
31039225+jessegoodier@users.noreply.github.com <31039225+jessegoodier@users.noreply.github.com>
2021-03-14 15:31:44 -0400

    fix for macs

--- run.sh
+++ run.sh
@@ -3,17 +3,20 @@ set -e
 source .env
 perl -i -pe s/yourpasswordhere/$PASSWORD/g ./fluentd/fluent-app-protect.conf
 
-if [ `cat /proc/sys/vm/max_map_count` -lt 262144 ]
-then
-  echo "max_map_count too low. run:"
-  echo "sudo sysctl -w vm.max_map_count=262144"
-  echo "and this to persist reboot:"
-  echo "echo \"vm.max_map_count=262144\" | sudo tee -a /etc/sysctl.conf"
-  echo "then run this script again"
-  exit
+# on linux, check for vm.max_map_count
+if test -f /proc/sys/vm/max_map_count ; then
+  if [ `cat /proc/sys/vm/max_map_count` -lt 262144 ]
+  then
+    echo "vm.max_map_count too low. run:"
+    echo "sudo sysctl -w vm.max_map_count=262144"
+    echo "and this to persist reboot:"
+    echo "echo \"vm.max_map_count=262144\" | sudo tee -a /etc/sysctl.conf"
+    echo "then run this script again"
+    exit
+  fi
 fi
 
-if [ $LOCAL_IP = "yourip" ]
+if [ "$LOCAL_IP" = "yourip" ]
 then
   echo "you must set your local ip in the .env file"
   exit
